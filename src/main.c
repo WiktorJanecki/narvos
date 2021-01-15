afbe06844a7152a7ad4fd82e92348924a5846402
Wiktor Janecki <j.wiktor05@interia.pl>
2021-01-15 13:53:09 +0100

    Add rendering system

--- src/main.c
+++ src/main.c
@@ -6,9 +6,22 @@
 #include "systems.h"
 
 int main(){
+    if(SDL_Init(SDL_INIT_EVERYTHING)){
+        printf("Failed to init sdl");
+        return 0;
+    }
+
     SDL_Window *window = SDL_CreateWindow("narvos",0,0,1280,720,0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
     SDL_Event event;
+
+    Entity_t entity;
+    TransformComponent_t transformComponent;
+    transformComponent.x = 64;
+    transformComponent.y = 96;
+    entity.components.transformComponent = &transformComponent;
+    
+    setSystemsRenderer(renderer);
 
     while(1){
         while(SDL_PollEvent(&event)){
@@ -17,10 +30,10 @@ int main(){
                 return 0;
             }
         }
+    
+        SDL_SetRenderDrawColor(renderer,0,0,0,0);
+        SDL_RenderClear(renderer);
+        updateSystems(entity);
+        SDL_RenderPresent(renderer);
     }
-
-
-
-
-
 }
